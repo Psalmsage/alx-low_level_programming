3ee92f5e4c4d6c904886972c56d2f87f78b9ae90
Psalm <Olonijayesamson@gmail.com>
2022-10-13 16:39:27 -0700

    c-programming

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,12 +1,12 @@
 #include<stdio.h>
 int main(void)
 {
-	printf("size of char is=%i byte\n" sizeof(char));
-	printf("size of int is=%i byte\n" sizeof(int));
-	printf("size of signed char is=%i byte\n" sizeof(signed char));
-	printf("size of unsigned char is=%i byte\n" sizeof(unsigned char));
-	printf("Size of a long int is=%i byte\n" sizeof(long int));
-	printf("Size of a long long int is=%i byte\n" sizeof(long long int));
-	printf("Size of a float is=%i byte\n" sizeof(float));
+	printf("size of char is=%i byte\n" ,sizeof(char));
+	printf("size of int is=%i byte\n" ,sizeof(int));
+	printf("size of signed char is=%i byte\n" ,sizeof(signed char));
+	printf("size of unsigned char is=%i byte\n" ,sizeof(unsigned char));
+	printf("Size of a long int is=%i byte\n" ,sizeof(long int));
+	printf("Size of a long long int is=%i byte\n" ,sizeof(long long int));
+	printf("Size of a float is=%i byte\n" ,sizeof(float));
 	return 0;
 }
