dd1227b8acd66bb8450cb02d16aa2aa2f6e2cd69
Psalm <Olonijayesamson@gmail.com>
2022-10-15 18:16:22 -0700

    been positive

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -4,6 +4,13 @@
 /* more headers goes there */
 
 /* betty style doc for function main goes there */
+/**
+ * main- main start
+ * @main - postivity and negativity
+ * Return: Always 0 (Success)
+ * To print out the positivity and negativity in the variable n
+ * main - This is the start main
+ */
 int main(void)
 {
 	int n;
