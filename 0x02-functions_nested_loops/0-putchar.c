0129864bd9bff35eaef1f91df9c3e75ffddfb739
Psalm <Olonijayesamson@gmail.com>
2022-10-18 13:08:49 -0700

    functions

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+/* betty style doc for function main goes there */
+/**
+ * main- main start
+ * @main - for alphabet
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int ch;
+
+	ch = '_';
+	putchar(ch);
+	ch = 'p';
+	putchar(ch);
+	ch = 'u';
+	putchar(ch);
+	ch = 't';
+	putchar(ch);
+	ch = 'c';
+	putchar(ch);
+	ch = 'h';
+	putchar(ch);
+	ch = 'a';
+	putchar(ch);
+	ch = 'r';
+	putchar(ch);
+	putchar('\n');
+
+	return (0);
+}
