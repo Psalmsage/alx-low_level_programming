fe05c9be2eeeaeb66fb029896d6e61feb633aa02
Psalm <Olonijayesamson@gmail.com>
2022-10-18 14:10:15 -0700

    functions

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,14 +1,12 @@
 #include <stdio.h>
-#include "main.h"
 /* betty style doc for function main goes there */
 /**
- * print_alphabet - for alphabet
  * main - check the code
  * @main - for alpha
  * Return: Always 0 (Success)
  */
 
-int print_alphabet(void)
+int main(void)
 {
 	int ch;
 
