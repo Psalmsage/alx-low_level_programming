8e9807f2a91ff99d96ab734167a5792024f762ee
Psalm <Olonijayesamson@gmail.com>
2022-10-25 03:26:36 -0700

    pointers

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * reset_to_98 - to reset values
+ *
+ * @m : for all m
+ *
+ * Return: Always 0 (Success)
+ */
+void reset_to_98(int *m)
+{
+	int n;
+
+	m = &n;
+	*m = 98;
+	printf("n=%d\n", n);
+}
