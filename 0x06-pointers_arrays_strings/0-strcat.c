535e5a3ba8881ae66ba5c37f3244c0d859dc9d0d
Psalm <Olonijayesamson@gmail.com>
2022-10-28 11:04:05 -0700

    pointers for array

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -4,23 +4,29 @@
  * _strcat - for the test
  * @dest: for the destination
  * @src: for the source
+ *
  * Return: Always 0 (Success)
  */
 
 char *_strcat(char *dest, char *src)
 {
-	while (*dest != '\0')
+	int i;
+	int j;
+
+	i = 0;
+	while (dest[i] != '\0')
 	{
-		dest++;
+		i++;
 	}
 
-	while (*src != '\0')
+	j = 0;
+	while (src[j] != '\0')
 	{
-		*dest = *src;
-		dest++;
-		src++;
+		dest[i] = src[j];
+		i++;
+		j++;
 	}
-	*dest = '\0';
-	return (0);
+	dest[i] = '\0';
+	return (dest);
 
 }
