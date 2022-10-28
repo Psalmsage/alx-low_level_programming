1967610fff6000dd21357963237770c8385e7d8c
Psalm <Olonijayesamson@gmail.com>
2022-10-28 11:21:03 -0700

    pointer for array

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,27 @@
+/**
+ * _strncpy - for strip
+ * @dest: destination
+ * @src: source
+ * @n: number
+ * Return: Always dest (Success)
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int j;
+
+	j = 0;
+	while (j < n && src[j] != '\0')
+	{
+		dest[j] = src[j];
+		j++;
+	}
+	while (j < n)
+	{
+		dest[j] = '\0';
+		j++;
+	}
+
+	return (dest);
+}
+
