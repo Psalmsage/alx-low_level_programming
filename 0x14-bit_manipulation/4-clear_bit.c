a77f59c4081f8948ee10b23347eb8de0b2d02b33
Psalm <Olonijayesamson@gmail.com>
2022-12-01 19:03:23 -0800

    bit - manipulation

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * clear_bit - Sets the value of a bit
+ * @n: A pointer to the bit
+ * @index: The index
+ *
+ * Return: If an error occurs - -1.
+ */
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	if (index >= (sizeof(unsigned long int) * 8))
+		return (-1);
+	*n &= ~(1 << index);
+
+	return (1);
+}
